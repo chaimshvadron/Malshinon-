4e48f3a3ed0286407d216bd3ffc2c35284afa388
ChaimShvadron <s0527649958@gmail.com>
2025-06-10 21:18:47 +0300

    Implement report submission functionality and add helper methods for person management

---
+++ Services/ReportService.cs
@@ -0,0 +1,17 @@
+using Malshinon.DB;
+using Malshinon.models;
+using Malshinon.Utils;
+using Malshinon.DAL;
+using ZstdSharp.Unsafe;
+
+namespace Malshinon.Services
+{
+    public class ReportService
+    {
+        public void AddNewPeople(string type)
+        {
+
+        }
+
+    }
+}
